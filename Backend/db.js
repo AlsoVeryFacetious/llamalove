1848f9f92478c33622ba4d3d007028f5a95608e9
AlsoVeryFacetious <zsalabye@student.neumont.edu>
2021-11-11 19:01:10 -0700

    Here is the project now with a database connection using MongoDB Atlas. db.js is the database file.

---
+++ Backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const url = `mongodb+srv://zsalabye:xmasgift12@llamacluster.ug7af.mongodb.net/LlamaDB?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
