099bc16f3c0479f196d1ee04e1fc22c21fe64314
AlsoVeryFacetious <zsalabye@student.neumont.edu>
2021-11-17 19:26:32 -0700

    Here is the updated backend with deleted files and unused folders.

--- Backend/models.js
+++ Backend/models.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const internal = require('stream');
 
 const userSchema = new mongoose.Schema({
     name: String,
     username: String,
     password: String
+});
+
+const questionSchema = new mongoose.Schema({
+    
 });
 
 exports.User = mongoose.model("User", userSchema);
