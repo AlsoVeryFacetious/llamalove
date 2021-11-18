f4e1cc2915b7278d25a552f84d64d0c3be77e480
DanielG-N <dagonzalez@student.neumont.edu>
2021-11-17 19:03:52 -0700

    login + salting and hashing passwords :)

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,5 +1,6 @@
 const models = require('../models');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const url = `mongodb+srv://zsalabye:xmasgift12@llamacluster.ug7af.mongodb.net/LlamaDB?retryWrites=true&w=majority`;
 
@@ -7,6 +8,7 @@ const connectionParams={
     useNewUrlParser: true,
     useUnifiedTopology: true 
 }
+
 mongoose.connect(url,connectionParams)
     .then( () => {
         console.log('Connected to database ')
@@ -16,8 +18,10 @@ mongoose.connect(url,connectionParams)
     });
 
 exports.createUser = async (req, res) => {
+    let salt = bcrypt.genSaltSync(10);
     console.log(req.body)
-    const user = new models.User(req.body);
+    const user = models.User(req.body);
+    user.password = bcrypt.hashSync(user.password, salt);
     try{
         await user.save();
         res.send(user);
@@ -27,7 +31,18 @@ exports.createUser = async (req, res) => {
     }
 }
 
-exports.getUsers = async (req, res) =>{
-    const users = await models.User.find({});
+exports.login = async (req, res) => {
+    const userName = req.body.username;
+    const password = req.body.password;
+    const user = await models.User.findOne({username: userName});
+    console.log(user);
+    if (bcrypt.compareSync(password, user.password)){
+        console.log("logged in :)");
+        res.send("logged in :)");
+    }
+}
+
+exports.getUsers = async (req, res) => {
+    const users = await models.User.find({}).lean();
     res.send(users);
 }
